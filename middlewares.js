38e0c1a47f5748bfad37288698d927586afea143
changhoi <0417kch@naver.com>
2019-05-04 20:47:47 +0900

    JWT 하는 중

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,5 @@
+import jwt from 'jsonwebtoken';
+
 export const isNotLoggedIn = (req, res, next) => {
   // 지금 로그인 안한 상태: next
   if (!req.user) next();
@@ -10,4 +12,22 @@ export const isLoggedIn = (req, res, next) => {
   // 지금 로그인 한 상태: next
   if (req.user) next();
   else res.status(401).redirect('/');
+};
+
+export const verifyToken = (req, res, next) => {
+  try {
+    req.decode = jwt.verify(req.headers.authorization, process.env.JWT_SECRET);
+    return next();
+  } catch (e) {
+    if (e.name === 'TokenExpiredError') {
+      return res.status(419).json({
+        code: 419,
+        message: '토큰이 만료되었습니다'
+      });
+    }
+    return res.status(401).json({
+      code: 401,
+      message: '유효하지 않은 토큰입니다.'
+    });
+  }
 };
